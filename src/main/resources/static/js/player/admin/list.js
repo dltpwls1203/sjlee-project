f2a9880627db5daa001a8485851af1661744aaa0
isjin <dltpwls1203@gamil.com>
2026-07-30 14:14:55 +0900

    선수 목록 상세 버튼 추가

--- src/main/resources/static/js/player/admin/list.js
+++ src/main/resources/static/js/player/admin/list.js
@@ -61,7 +61,7 @@ function renderPlayerList(players, page, size) {
     if (!players || players.length === 0) {
         tbody.innerHTML = `
             <tr>m
-                <td colspan="6" class="text-center">등록된 선수가 없습니다.</td>
+                <td colspan="7" class="text-center">등록된 선수가 없습니다.</td>
             </tr>
         `;
         return;
@@ -84,6 +84,14 @@ function createPlayerRow(player, index, page, size) {
             <td>${player.position ?? "-"}</td>
             <td>${player.backNumber ?? "-"}</td>
             <td>${player.attendCount ?? 0}</td>
+            <td class="text-center">
+                <button type="button"
+                        class="btn btn-sm btn-outline-primary"
+                        title="선수 수정"
+                        onclick="goToPlayerDetail(${player.playerId})">
+                    <i class="fas fa-edit"></i> 수정
+                </button>
+            </td>
         </tr>
     `;
 }
@@ -112,4 +120,8 @@ function getKoreanAge(birthDate) {
     const age = baseYear - birthYear + 1;
 
     return `${age}세`;
+}
+
+function goToPlayerDetail(playerId) {
+    location.href = `/admin/players/detail/${playerId}`;
 }
